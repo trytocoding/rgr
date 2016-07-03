d919a739ea0adc98f7229daf24a159d24400eb32
Eugene <sinnersaint0@gmail.com>
2016-07-03 13:21:12 +0300

    added comments int Alts

---
+++ Alt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ras4etka
+{
+    public class Alt //public class for alts
+    {
+        private int AltID;
+        private string AltName;
+
+        public Alt(int ID, string Name)
+        {
+            AltID = ID;
+            AltName = Name;
+        }
+        public string GetName()
+        {
+            return this.AltName;
+        }
+        public int GetID()
+        {
+            return this.AltID;
+        }
+            
+    }
+}
